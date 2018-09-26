24b03532ebfab08194032484e8bd4e484e147ffa
Naz Al Kassm <pal.kassm@hotmail.com>
2018-09-26 17:34:43 -0400

    Add PlayerTest, Add StockTest
    Following TDD, I created these test classes according to the game scenario I have. The test covers most of the functionality. I am still working on them. While creating the tests. I have created all supporting dummy methods and object that will be implemented later. My next Commit, will add the dummy classes.

---
+++ src/test/java/comp/rummikub/PlayerTest.java
@@ -0,0 +1,44 @@
+package comp.rummikub;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+	
+	private Player player1;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void playerFillRackTest() 
+	{
+		
+	}
+	
+	@Test
+	void player()
+	{
+		
+	}
+	
+
+}
--- src/test/java/comp/rummikub/StockTest.java
+++ src/test/java/comp/rummikub/StockTest.java
@@ -2,18 +2,23 @@ package comp.rummikub;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import com.rummikub.*;
+
+
 
 class StockTest {
 	
 	private Stock stock1;
 	private Stock stock2 = new Stock(stock1);
+	private Player player1;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception 
@@ -38,6 +43,13 @@ class StockTest {
 	{
 		
 	}
+	
+	@Test
+	void createStockTest()
+	{
+		stock1.createStock();
+		assertThat(stock1.getStock().isEmpty(), is(false));
+	}
 
 	@Test
 	void shuffleTest() 
@@ -53,12 +65,30 @@ class StockTest {
 		        //stock2.shuffle();
 		        assertFalse(stock1.equals(stock2));
 		  }
+		  
+		  for(int t=0; t < stock1.getLength(); t++)
+		  {
+			  int sameTiles = 0;
+			  if(stock1.getStock().get(t).equals(stock2.getStock().get(t)))
+			  {
+				  sameTiles++;
+			  }
+			  else
+			  {
+			  assertThat(stock1.getStock().get(t),is(not(stock2.getStock().get(t))));
+			  }
+			  System.out.println("The number of same tiles after shuffling is : " + sameTiles);
+		  }		  
 	}
 	
+
 	@Test
-	void dealHandTest()
+	void dealRackTest()
 	{
-		//assetThat(playerHand.size,is(14));
+		int stockSizeBeforeDealing = stock1.getStock().size();
+		player1.fillRack(stock1.dealRack());
+		assertThat(player1.getRack(),is(notNullValue()));
+		assertThat(stockSizeBeforeDealing-14,is(player1.getRack().getSize()));
 	}
 	
 	@Test
@@ -67,6 +97,9 @@ class StockTest {
 		//int playerCardN = player1.playerHand.size;
 		//player1.dealTile();
 		//assertThat(playerCardN,++player1.playerHand.size);
+		//assertThat(stock1.size,is(103));
 	}
+	
+	
 	
 }
