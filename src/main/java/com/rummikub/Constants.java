9075d859ea6b917df7fdfa8fbb001fb24f3fedfe
Naz Al Kassm <pal.kassm@hotmail.com>
2018-10-10 20:25:05 -0400

    Add Constants Class
    Added a Constant class where we put all our game constants. All constants are static finals and the class is final itself so it does not get inherited. Also, it has a private constructor so no one tries and instantiate it.
    Example are : STOCK_SIZE = 104, RACK_SIZE 14.

---
+++ src/main/java/com/rummikub/Constants.java
@@ -0,0 +1,14 @@
+package com.rummikub;
+
+public final class Constants 
+{
+	//Can't be inherited
+	private Constants()
+	{
+		//Don't let anyone instantiate this class
+	}
+	
+	public static final int RACK_SIZE = 14;
+	public static final int PLAYERS_NUMBER = 4;
+	public static final int STOCK_SIZE = 104;
+}
