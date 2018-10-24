e5e23ac8963ca74ff96d2576e5ae916f13715b2a
Shiraj Gill <shirajgill@cmail.carleton.ca>
2018-10-23 20:42:45 -0400

    Behaviour added extra functions (Done with Prady)

--- src/main/java/com/rummikub/Behaviour.java
+++ src/main/java/com/rummikub/Behaviour.java
@@ -2,4 +2,5 @@ package com.rummikub;
 
 public interface Behaviour {
 	public void play(boolean canPlayOnTableMelds, Rack playerRack, Table table);
+	public void canPlayOnTableMelds(boolean canPlayOnTableMelds, Rack playerRack, Table table);
 }
