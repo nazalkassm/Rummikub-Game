f935c27c40c23a328de9d02bc96b0ca4f5f677e3
Naz Al Kassm <pal.kassm@hotmail.com>
2018-11-02 19:44:42 -0400

    Strategy3Test is done
    This strategy3test is done but it is not optimized to test for a strategy that does use the table to play. We have to update this test when we allow players to play from table.

--- src/test/java/com/rummikub/Strategy3Test.java
+++ src/test/java/com/rummikub/Strategy3Test.java
@@ -23,13 +23,14 @@ class Strategy3Test {
 	private static Player player1;
 	private static Player player2;
 	private static Player player3;
+	private static Table table;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception 
 	{
 		//Table because strategy 3 needs to observe
 		Stock stock = new Stock();
-		Table table = new Table(stock);
+		table = new Table(stock);
 		
 		//player1 has tiles greater than 30 and can play them.
 		player1 = new Player("Naz",new Strategy3());
@@ -56,7 +57,6 @@ class Strategy3Test {
 		player2.getPlayerRack().addTile(new Tile("O", "4")); //G6
 		
 		//player 3 
-		player3.getPlayerRack().addTile(new Tile("G", "10")); //G5
 		player3.getPlayerRack().addTile(new Tile("G", "11")); //G6
 		player3.getPlayerRack().addTile(new Tile("G", "12")); //G6
 		player3.getPlayerRack().addTile(new Tile("R", "9"));  //G4
@@ -99,16 +99,47 @@ class Strategy3Test {
 		assertEquals(7,player1.getPlayerRack().getSize());
 		assertEquals(8,player2.getPlayerRack().getSize());
 		
-		//player1 tests
+		//player1 plays all his tiles in one shot test
 		assertEquals("[G10 G11 G12 , R9 R10 R11 R12 ]", player1.play().toString());
 		assertEquals(0,player1.getPlayerRack().getSize());
 		
-		//player2 tests
-		assertEquals(Collections.emptyList() ,player2.play());
+		//player2 does not play anything cuz < 30
+		assertEquals(Collections.emptyList(), player2.play());
 		assertEquals(8,player2.getPlayerRack().getSize());
 		
-		//player3 tests (this tests has 3 fewer tiles).
-		player3.
+		//We dont want other players to have 3 fewer so we add tiles
+		player1.getPlayerRack().addTile(new Tile("G", "10")); //G5
+		player1.getPlayerRack().addTile(new Tile("G", "11")); //G6
+		player1.getPlayerRack().addTile(new Tile("G", "12")); //G6
+		player1.getPlayerRack().addTile(new Tile("R", "9")); //G4
+		player1.getPlayerRack().addTile(new Tile("R", "10")); //G5
+		player1.getPlayerRack().addTile(new Tile("R", "11")); //G6
+		player1.getPlayerRack().addTile(new Tile("R", "12")); //G6
 		
+		//table.notifyObservers(); <-- not going to notify to test that the p3 actually still plays his initial turn even if a player has 3 fewer.
+		//player3 plays his initial meld of > 30
+		assertEquals("[R9 R10 R11 R12 ]", player3.play());
+		assertEquals(2,player2.getPlayerRack().getSize());
+		//player 3 now plays again (knowing he played his first 30). 
+		//He has to check if other players have 3 fewer tiles than him.
+		//player 3 add 4 tiles now he has 6 , player 1 has 7 , and player 2 has 8
+		player3.getPlayerRack().addTile(new Tile("R", "4")); 
+		player3.getPlayerRack().addTile(new Tile("B", "4")); 
+		player3.getPlayerRack().addTile(new Tile("G", "4")); 
+		player3.getPlayerRack().addTile(new Tile("O", "4")); 
+		
+		// update the info so that player 3 knows that no other player has 3 fewer
+		table.notifyObservers();
+		//Size should not change because player does not play anything from his rack since not fewer than 3
+		assertEquals(Collections.emptyList(),player3.play());
+		assertEquals(6,player3.getPlayerRack().getSize());
+		//now we make it so that there is a player with fewer 3 by doing:
+		player1.play();
+		
+		//update lowest count
+		table.notifyObservers();
+		//player1 should have 0 tiles now player3 will play every meld he has :
+		assertEquals(Collections.emptyList(),player3.play());
+		assertEquals(2,player3.getPlayerRack().getSize());
 	}
 }
