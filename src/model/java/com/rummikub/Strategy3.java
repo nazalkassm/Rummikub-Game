5452aea72295ab93035bf4573f154c1cfc9608a6
Pradyumna Rao <pradyraoofficial@gmail.com>
2018-11-03 17:49:17 -0400

    fixed strategy 3 to play its inital 30 cards asap

--- src/model/java/com/rummikub/Strategy3.java
+++ src/model/java/com/rummikub/Strategy3.java
@@ -25,60 +25,72 @@ public class Strategy3 implements StrategyBehaviour {
 
 		//print table and possible melds
 		Print.printRacktoUser(currPlayer.getPlayerRack(),currPlayer.isPrint_rack_meld());
-		Print.printMeldtoUser(possibleMelds);
-
+		Print.printMeldtoUser(possibleMelds,currPlayer.isPrint_rack_meld());
 		
-		//execute play logic for this strategy
-		playStrategy(currPlayer, possibleMelds, returnMelds);
-		
-		
-		//checks for sum of returning melds
-		for (Meld m: returnMelds) {
-			sum += m.sumMeld();
-		}
-
 		//checks if player has already played its initial 30
 		//if it hasn't then it checks whether the playable meld's sum is 30 or greater
-		//if either true, returns played melds and ends turn
-		if(currPlayer.canPlayOnExistingMelds || sum >= 30) {
-			currPlayer.canPlayOnExistingMelds = true;
-			return returnMelds;
+		//
+		if(!currPlayer.canPlayOnExistingMelds) {
+			logic(currPlayer, possibleMelds, returnMelds);
+			if(getSum(sum, returnMelds) >= 30){
+				currPlayer.canPlayOnExistingMelds = true;
+				return returnMelds;
+			}
 		}
 
+		//if either true, returns played melds and ends turn
+		if(currPlayer.canPlayOnExistingMelds){
+			playStrategy(currPlayer, possibleMelds, returnMelds);
+			return returnMelds;
+		}
+		
 		//if player has not played inital 30 AND playable melds sums less than 30
 		//player cannot place playable melds on table
 		//so player's rack gets reset to when the turn started and ends turn
-		else {
-			Print.print("You cannot play your initial 30 this round");
-			currPlayer.getPlayerRack().setRack(tempList);
-			return Collections.emptyList(); 
-		}
+		Print.print("Player 4 tried playing melds but the sum is < 30.");
+		currPlayer.getPlayerRack().setRack(tempList);
+		return Collections.emptyList(); 
 	}
 	
 	@Override
 	public void playStrategy(Player currPlayer, List<Meld> possibleMelds, List<Meld> returnMelds) {
 		if(tableInfo.getLowestHandCount() <= currPlayer.getPlayerRack().getSize() - 3) {
-			while (possibleMelds.size() > 0) {
-				//now add Meld with max sum to return melds
-				returnMelds.add(possibleMelds.get(Meld.getMaxIndex(possibleMelds)));
-				//pop the tiles with were added to return melds
-				currPlayer.getPlayerRack().removeTiles(possibleMelds.get(Meld.getMaxIndex(possibleMelds)));
-				//update possible melds to get new list of melds
-				possibleMelds = new ArrayList<>(currPlayer.getPlayerRack().getMelds());
-				
-				//print updated rack and possible melds to UI
-				Print.printRacktoUser(currPlayer.getPlayerRack(),currPlayer.isPrint_rack_meld());
-				Print.printMeldtoUser(possibleMelds);
-			}
+			logic(currPlayer, possibleMelds, returnMelds);
 			if (possibleMelds.size() == 0) {
 				//play using the table
 			}
 		}
 		else {
 			//play only table cards 
+					
 		}
 	}
-
+	
+	public void logic(Player currPlayer, List<Meld> possibleMelds, List<Meld> returnMelds) {
+		while (possibleMelds.size() > 0) {
+			//now add Meld with max sum to return melds
+			returnMelds.add(possibleMelds.get(Meld.getMaxIndex(possibleMelds)));
+			//pop the tiles with were added to return melds
+			currPlayer.getPlayerRack().removeTiles(possibleMelds.get(Meld.getMaxIndex(possibleMelds)));
+			//update possible melds to get new list of melds
+			possibleMelds = new ArrayList<>(currPlayer.getPlayerRack().getMelds());
+			
+			//print updated rack and possible melds to UI
+			Print.printRacktoUser(currPlayer.getPlayerRack(),currPlayer.isPrint_rack_meld());
+			Print.printMeldtoUser(possibleMelds,currPlayer.isPrint_rack_meld());
+		}
+	}
+	
+	public int getSum(int sum, List<Meld> returnMelds) {
+		//checks for sum of returning melds
+		for (Meld m: returnMelds) {
+			sum += m.sumMeld();
+		}
+		
+		return sum;
+	}
+	
+	
 	@Override
 	public void update(TableInfo tableInfo) 
 	{
