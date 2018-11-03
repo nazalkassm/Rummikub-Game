90a9f114c041a3e0e8a4f8d6bd2b445f2390a0bc
ElijahMacPherson <elijahmacpherson@cmail.carleton.ca>
2018-11-03 18:46:55 -0400

    Created additional test plans

--- src/test/java/com/rummikub/ScenarioTest.java
+++ src/test/java/com/rummikub/ScenarioTest.java
@@ -6,14 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ScenarioTest {
-	private static Game game;
+	private Game game;
 	
-	@BeforeAll
-	static void setUpClass() {
+	@BeforeEach
+	void setUpClass() {
 		game = new Game();
+		game.printRackMeld = true;
+		game.waitAferEachTurn = false;
 	}
 	
 	@Test
@@ -42,9 +45,7 @@ public class ScenarioTest {
 	
 	@Test
 	void Scenerio2Test() {
-		// 4.c)
-		Game game = new Game();
-		
+		// 4.c)		
 		FileParser.parse("src/main/resources/inputFiles/test2.txt");
 		assertFalse(FileParser.inputError);
 		
@@ -52,7 +53,6 @@ public class ScenarioTest {
 		game.table = new Table(game.stock);
 		
 		assertEquals(60, game.stock.getLength());
-		//assertEquals(3, FileParser.playerCommands.size());
 		
 		try {
 			Prompt.init(FileParser.playerCommands);
@@ -69,16 +69,14 @@ public class ScenarioTest {
 	@Test
 	void Scenerio3Test() {
 		// 4.c)
-		Game game = new Game();
-		
 		FileParser.parse("src/main/resources/inputFiles/test3.txt");
 		assertFalse(FileParser.inputError);
 		
 		game.stock = FileParser.stock;
 		game.table = new Table(game.stock);
 		
+		
 		assertEquals(60, game.stock.getLength());
-		//assertEquals(3, FileParser.playerCommands.size());
 		
 		try {
 			Prompt.init(FileParser.playerCommands);
@@ -95,7 +93,6 @@ public class ScenarioTest {
 	@Test
 	void Scenerio4Test() {
 		// 4.c)
-		Game game = new Game();
 		
 		FileParser.parse("src/main/resources/inputFiles/test4.txt");
 		assertFalse(FileParser.inputError);
@@ -104,13 +101,56 @@ public class ScenarioTest {
 		game.table = new Table(game.stock);
 		
 		assertEquals(60, game.stock.getLength());
-		//assertEquals(3, FileParser.playerCommands.size());
 		
 		try {
 			Prompt.init(FileParser.playerCommands);
 			game.start();
 			
 			assertEquals("Player 4", game.winner.getName());
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		Prompt.init();
+	}
+	
+	@Test
+	void Scenerio5Test() {
+		// 4a1, 8b, 10a
+		
+		FileParser.parse("src/main/resources/inputFiles/test5.txt");
+		assertFalse(FileParser.inputError);
+		
+		game.stock = FileParser.stock;
+		game.table = new Table(game.stock);
+		
+		assertEquals(60, game.stock.getLength());
+		
+		try {
+			Prompt.init(FileParser.playerCommands);
+			game.start();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		Prompt.init();
+	}
+	
+	@Test
+	void Scenerio6Test() {
+		// 4a2, 8a
+		
+		FileParser.parse("src/main/resources/inputFiles/test6.txt");
+		assertFalse(FileParser.inputError);
+		
+		game.stock = FileParser.stock;
+		game.table = new Table(game.stock);
+		
+		assertEquals(60, game.stock.getLength());
+		
+		try {
+			Prompt.init(FileParser.playerCommands);
+			game.start();
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
