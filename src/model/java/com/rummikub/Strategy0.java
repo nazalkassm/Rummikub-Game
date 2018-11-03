043588f85b6676e148fd3981abf4af13857c746c
Naz Al Kassm <pal.kassm@hotmail.com>
2018-11-03 01:42:26 -0400

    Ignore

--- src/model/java/com/rummikub/Strategy0.java
+++ src/model/java/com/rummikub/Strategy0.java
@@ -48,7 +48,7 @@ public class Strategy0 implements StrategyBehaviour
 		//player cannot place playable melds on table
 		//so player's rack gets reset to when the turn started and ends turn
 		else {
-			Print.print("You cannot play your initial 30 this round");
+			Print.print("There were no melds played this round because your melds do not sum up to 30.");
 			currPlayer.getPlayerRack().setRack(tempList);
 			return Collections.emptyList(); 
 		}
