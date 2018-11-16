c7db1cd9115b8248020f6e4aa4aee540cefc7433
Shiraj Gill <shirajgill@cmail.carleton.ca>
2018-11-15 19:21:17 -0500

    Memento added to table

--- src/model/java/com/rummikub/TableInfo.java
+++ src/model/java/com/rummikub/TableInfo.java
@@ -3,6 +3,8 @@ package com.rummikub;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.rummikub.Player.Memento;
+
 public class TableInfo 
 {
 	
@@ -20,6 +22,33 @@ public class TableInfo
 	
 	public List<Meld> getMelds() {
 		return melds;
+	}
+	
+	public Memento saveToMemento() {
+		System.out.println("Saving time to Memento");
+		return new Memento(this.melds);
+	}
+
+	public void restoreFromMemento(Memento memento) {
+		this.melds = memento.getSavedMelds();
+	}
+
+	public static class Memento {
+		private final List<Meld> savedMelds;
+
+		public Memento(List<Meld> meldsToSave) {
+			//Melds to save is a new list of melds 
+			this.savedMelds = new ArrayList<Meld>();
+			
+			for (Meld meld : meldsToSave) {
+				this.savedMelds.add(new Meld((Tile[]) meld.getMeld().toArray()));
+			}
+		}
+
+		// accessible by outer class only
+    private List<Meld> getSavedMelds() {
+        return savedMelds;
+    }
 	}
 
 }
