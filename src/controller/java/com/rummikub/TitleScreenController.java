c30594a451a1f3f3c2801aa928ae74b224bb71fd
Pradyumna Rao <pradyraoofficial@gmail.com>
2018-11-22 19:18:17 -0500

    with Elijah: Made a main screen for the game - screen has some basic labels - game runs in the console - next step is to update these labels after a player's turn ends

--- src/controller/java/com/rummikub/TitleScreenController.java
+++ src/controller/java/com/rummikub/TitleScreenController.java
@@ -1,26 +1,18 @@
 package com.rummikub;
 
-import static org.junit.Assert.assertArrayEquals;
-
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import com.sun.javafx.geom.AreaOp.AddOp;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 
 public class TitleScreenController implements Initializable {
 	@FXML
@@ -40,7 +32,6 @@ public class TitleScreenController implements Initializable {
 	@FXML
 	private Button btn_Play;
 
-	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		cb_PlayerCount.getItems().addAll("2", "3", "4");
@@ -48,98 +39,95 @@ public class TitleScreenController implements Initializable {
 		cb_Player2.getItems().addAll("Human", "Strategy 1", "Strategy 2", "Strategy 3");
 		cb_Player3.getItems().addAll("Human", "Strategy 1", "Strategy 2", "Strategy 3");
 		cb_Player4.getItems().addAll("Human", "Strategy 1", "Strategy 2", "Strategy 3");
-		
+
 		for (int i = 0; i < vb_PlayerStrategies.getChildren().size(); i++) {
 			vb_PlayerStrategies.getChildren().get(i).setVisible(false);
 		}
 	}
-	
+
 	@FXML
 	public void handlePlayerCountCB(ActionEvent event) {
-		int numPlayers = Integer.parseInt(cb_PlayerCount.getValue());	
-		
+		int numPlayers = Integer.parseInt(cb_PlayerCount.getValue());
+
 		for (int i = 0; i < vb_PlayerStrategies.getChildren().size(); i++) {
-			ComboBox<String> currNode = (ComboBox<String>) vb_PlayerStrategies.getChildren().get(i); 
-			
+			ComboBox<String> currNode = (ComboBox<String>) vb_PlayerStrategies.getChildren().get(i);
+
 			if (i < numPlayers) {
 				currNode.setVisible(true);
-			}
-			else {
+			} else {
 				currNode.setVisible(false);
 			}
 			currNode.setValue("Select");
-			
+
 		}
 
 		btn_Play.setDisable(true);
 	}
-	
+
 	@FXML
 	public void handleStrategyCB(ActionEvent event) {
 		boolean check = true;
-		int numPlayers = Integer.parseInt(cb_PlayerCount.getValue());	
+		int numPlayers = Integer.parseInt(cb_PlayerCount.getValue());
 		for (int i = 0; i < numPlayers; i++) {
 			ComboBox<String> currNode = (ComboBox<String>) vb_PlayerStrategies.getChildren().get(i);
-			if (currNode.getValue() == "Select") check = false;
+			if (currNode.getValue() == "Select")
+				check = false;
 		}
-		
+
 		if (check) {
 			btn_Play.setDisable(false);
-		}
-		else {
+		} else {
 			btn_Play.setDisable(true);
 		}
 	}
-	
+
 	@FXML
 	public void handlePlayBtn(ActionEvent event) throws Exception {
-		int numPlayers = Integer.parseInt(cb_PlayerCount.getValue());	
-		List<Player> players = new ArrayList<Player>(); 
+		int numPlayers = Integer.parseInt(cb_PlayerCount.getValue());
+		List<Player> players = new ArrayList<Player>();
 		boolean error = false;
 
 		for (int i = 0; i < numPlayers; i++) {
 			ComboBox<String> currNode = (ComboBox<String>) vb_PlayerStrategies.getChildren().get(i);
-			
+
 			switch (currNode.getValue()) {
 			case "Human":
-				players.add(new Player("p" + i, new Strategy0())); 
+				players.add(new Player("p" + i, new Strategy0()));
 				break;
 			case "Strategy 1":
-				players.add(new Player("p" + i, new Strategy1())); 
+				players.add(new Player("p" + i, new Strategy1()));
 				break;
 			case "Strategy 2":
-				players.add(new Player("p" + i, new Strategy2())); 
+				players.add(new Player("p" + i, new Strategy2()));
 				break;
 			case "Strategy 3":
-				players.add(new Player("p" + i, new Strategy3())); 
+				players.add(new Player("p" + i, new Strategy3()));
 				break;
-				//case "Strategy 4":
-				//players.add(new Player("p" + i, new Strategy4())); 
-				//break;
+			// case "Strategy 4":
+			// players.add(new Player("p" + i, new Strategy4()));
+			// break;
 			default:
 				error = true;
 			}
 		}
 
 		if (!error) {
-			Game game = new Game(players);
-			
+			// Game game = new Game(players);
+			Rummy.players = players;
 			// Get the event's source stage, and set the scene to be the game.
-			Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 			stage.setScene(Rummy.loadScene("MainScreen.fxml"));
-			
-			try {
-				game.start();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		else {
+
+			// try {
+			// game.start();
+			// } catch (IOException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+			// }
+		} else {
 			Print.print("Unknown strategy selected.");
 		}
 
 	}
-	
-	
+
 }
--- src/view/java/com/rummikub/Rummy.java
+++ src/view/java/com/rummikub/Rummy.java
@@ -1,25 +1,18 @@
 package com.rummikub;
 
-import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.scene.text.*;
 
+public class Rummy extends Application {
 
-public class Rummy extends Application{
-	
+	public static List<Player> players;
+
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
@@ -28,23 +21,22 @@ public class Rummy extends Application{
 	public void start(Stage primaryStage) throws Exception {
 
 		primaryStage.setScene(loadScene("TitleScreen.fxml"));
-		//primaryStage.setResizable(false);
+		// primaryStage.setResizable(false);
 		primaryStage.show();
 	}
-	
-	public static Scene loadScene(String fxmlPath) throws Exception  {
+
+	public static Scene loadScene(String fxmlPath) throws Exception {
 		URL url = Rummy.class.getResource(fxmlPath);
 		Scene scene;
-		
+
 		if (url != null) {
 			FXMLLoader fxmlLoader = new FXMLLoader();
 			fxmlLoader.setLocation(url);
 			Pane root = fxmlLoader.load();
-			
+
 			scene = new Scene(root);
-		}
-		else {
-			throw new Exception("FXML file \"" + fxmlPath +  "\" does not exist.");
+		} else {
+			throw new Exception("FXML file \"" + fxmlPath + "\" does not exist.");
 		}
 		return scene;
 	}
