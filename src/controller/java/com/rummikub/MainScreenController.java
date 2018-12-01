928ec9fe9a868429aa817920fe115795fcd3b478
ElijahMacPherson <elijahmacpherson@cmail.carleton.ca>
2018-11-30 22:36:58 -0500

    GUI development w/ Prady: - We drew an image of iron man!

--- src/controller/java/com/rummikub/MainScreenController.java
+++ src/controller/java/com/rummikub/MainScreenController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
@@ -60,7 +61,12 @@ public class MainScreenController implements Initializable {
 		for (int i = 0; i < playerPanes.size(); i++) {
 			viewTiles(game.players.get(i), playerPanes.get(i));
 		}
+		
+		ImageView iv1 = new ImageView(new Image("http://icons.iconarchive.com/icons/kidaubis-design/cool-heroes/128/Ironman-icon.png"));
+        iv1.relocate(table_pane.getLayoutX(), table_pane.getLayoutY());
+        table_pane.getChildren().addAll(iv1);
 	}
+	
 
 	@FXML
 	public void handleEndTurnBtn(ActionEvent event) {
