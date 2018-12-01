d22f0257f5adbd7d6d3a1f46084246bad6294e90
Shiraj Gill <shirajgill@cmail.carleton.ca>
2018-11-30 19:16:24 -0500

    Joker and UML Initial

--- src/model/java/com/rummikub/Joker.java
+++ src/model/java/com/rummikub/Joker.java
@@ -1,48 +1,63 @@
 package com.rummikub;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Joker extends Tile
 {
 	//Array that holds the possible values this Joker can be played as at a certain time.
+	//Empty list means everything
 	private List<Tile> possibleTiles = new ArrayList<Tile>();
 	
-	public Joker(Colours colour, Ranks rank) 
-	{
+	public Joker(Colours colour, Ranks rank) {
 		super(colour, rank);
-		this.setJoker(true);
 	}
 	
-	public Joker()
-	{
+	public Joker() {
 		super ("J", "0");
-		this.setJoker(true);
-		initalPossibleTiles();
 	}
 	
-	public void initalPossibleTiles()
-	{
-		for(Colours c: Colours.values())
-		{
-			for(Ranks r:Ranks.values())
-			{
-				if(!(c.equals(Colours.JOKER) || r.equals(Ranks.JOKER)))
-				{
-				Joker j = new Joker(c,r);
-				possibleTiles.add(j);
-				}
-			}
+	//@overide 
+	public boolean isRunOn(Tile tile) {
+		//For empty possible tiles return true at all times 
+		if (possibleTiles.isEmpty()) {
+			return true;
 		}
+		//Loop over all the 
+		for (Tile possibleTile: possibleTiles) {
+			if (possibleTile.isRunOn(tile))
+					return true;
+		}
+		
+		return false;
 	}
 	
-	public void setPossibleTiles(String[]... tilesToAdd)
-	{
-		possibleTiles.clear();
-		
-		for(String[] s : tilesToAdd)
-		{
-			possibleTiles.add(new Tile (s[0],s[1]));
+	public void setPossibleTiles(Meld meldWithJoker, List<Tile> tilesOnTable) {
+		//Remove the tiles in the meld from the tiles on the table 
+		tilesOnTable.removeAll(meldWithJoker.getTiles());
+		List<Tile> tiles = meldWithJoker.getTiles();
+		int jokerIndex = tiles.indexOf(this);
+		if (Meld.checkMeldType(meldWithJoker.getTiles()) == Meld.MeldType.RUN) {
+			//If the joker is the first one get the next one 
+			if (jokerIndex == 0) {
+				possibleTiles.add(new Tile(tiles.get(1).getColour().getSymbol(), Integer.toString(tiles.get(1).getRank().getValue() - 1)));
+			} else {
+				possibleTiles.add(new Tile(tiles.get(jokerIndex - 1).getColour().getSymbol(), Integer.toString(tiles.get(jokerIndex - 1).getRank().getValue() + 1)));
+			}
+		} else if (Meld.checkMeldType(meldWithJoker.getTiles()) == Meld.MeldType.SET) {
+			int i = 0;
+			List<Colours> possibleColors = new ArrayList<>(Arrays.asList(Colours.values()));
+			for (Tile t: tiles) {
+				possibleColors.remove(t.getColour());
+				if (t == this)
+					i++;
+			}
+			for (Colours possibleColor: possibleColors)
+				possibleTiles.add(new Tile(possibleColor, tiles.get(i).getRank()));
+		} else {
+			System.out.println("You done fucked up");
+      System.exit(0); 
 		}
 	}
 	
