527e5b762567ebb97267559c31a0261cdedb3b00
Naz Al Kassm <pal.kassm@hotmail.com>
2018-12-05 04:21:12 -0500

    Change stock size to 106 With Elijah

--- src/main/java/com/rummikub/Constants.java
+++ src/main/java/com/rummikub/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
 
 	public static final int RACK_SIZE = 14;
 	public static final int PLAYERS_NUMBER = 4;
-	public static final int STOCK_SIZE = 104;
+	public static final int STOCK_SIZE = 106;
 	public static final int ONE_INDEX = 1;
 	public static final int ZERO_TILES = 0;
 	public static final double PROBABILITY_TO_PLAY = 0.015;
